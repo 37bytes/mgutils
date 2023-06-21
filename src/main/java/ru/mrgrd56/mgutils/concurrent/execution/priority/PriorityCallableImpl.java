8d887450d7128c7ad7cb63b6bb2a7c7d78d7a64e
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-21 23:56:28 +0500

    Release v1.1

---
+++ src/main/java/ru/mrgrd56/mgutils/concurrent/execution/priority/PriorityCallableImpl.java
@@ -0,0 +1,26 @@
+package ru.mrgrd56.mgutils.concurrent.execution.priority;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @since 1.1
+ */
+class PriorityCallableImpl<T> implements PriorityCallable<T> {
+    private final int priority;
+    private final Callable<T> source;
+
+    public PriorityCallableImpl(int priority, Callable<T> source) {
+        this.priority = priority;
+        this.source = source;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public T call() throws Exception {
+        return source.call();
+    }
+}
