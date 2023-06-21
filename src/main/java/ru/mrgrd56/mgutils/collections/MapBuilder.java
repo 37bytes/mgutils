46e948d62c741afd76a0b86eb508a210039af6d2
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-21 21:18:44 +0500

    Pre v1.0

---
+++ src/main/java/ru/mrgrd56/mgutils/collections/MapBuilder.java
@@ -0,0 +1,132 @@
+package ru.mrgrd56.mgutils.collections;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Provides an interface for creating a mutable {@link Map}.<br>
+ * In created {@link Map}s, both keys and values can be {@code null}, unlike {@link Map#of} and {@link Map#ofEntries}.<br>
+ * The created implementation of {@link Map} can be manually specified, by default the {@link HashMap} is used.
+ * @param <K> The type of the key.
+ * @param <V> The type of the value.
+ * @since 1.0
+ */
+public class MapBuilder<K, V> {
+    private final Map<K, V> map;
+
+    /**
+     * Creates an instance of {@link MapBuilder} using the {@link HashMap} implementation.
+     * @since 1.0
+     */
+    public MapBuilder() {
+        this(HashMap::new);
+    }
+
+    /**
+     * Creates an instance of {@link MapBuilder} using the provided {@code mapFactory} to create a {@link Map}.<br>
+     * The {@link Map} created by {@code mapFactory} must support the {@link Map#put} operation.
+     * @param mapFactory The object that creates a {@link Map} instance.
+     * @since 1.0
+     */
+    public MapBuilder(Supplier<Map<K, V>> mapFactory) {
+        this.map = mapFactory.get();
+    }
+
+    /**
+     * Inserts an entry into the created {@link Map}, using the provided {@code key} and {@code value}.
+     * @param key The key associated with the value.
+     * @param value The value.
+     * @since 1.0
+     */
+    public MapBuilder<K, V> put(@Nullable K key, @Nullable V value) {
+        map.put(key, value);
+        return this;
+    }
+
+    /**
+     * Inserts an entry into the created {@link Map}, using the provided {@code key} and {@code value}.
+     * @param entry The entry added to the created {@link Map}.
+     * @since 1.0
+     */
+    public MapBuilder<K, V> put(@NotNull Map.Entry<K, V> entry) {
+        return put(entry.getKey(), entry.getValue());
+    }
+
+    /**
+     * Returns the created {@link Map} instance, the implementation depends on the {@code mapFactory}
+     * passed to the {@link MapBuilder#MapBuilder(Supplier)} constructor.
+     * @since 1.0
+     */
+    public Map<K, V> build() {
+        return map;
+    }
+
+    /**
+     * Creates an {@link Entry} instance.
+     * @since 1.0
+     */
+    public static <K, V> Map.Entry<K, V> entry(@Nullable K key, @Nullable V value) {
+        return new Entry<>(key, value);
+    }
+
+    /**
+     * Creates a {@link HashMap} instance, populating it with the provided {@code entries}.
+     * @since 1.0
+     */
+    @SafeVarargs
+    public static <K, V> Map<K, V> fromEntries(@NotNull Map.Entry<K, V>... entries) {
+        return fromEntriesInternal(new MapBuilder<>(), entries);
+    }
+
+    /**
+     * Creates a {@link Map} instance using the specified {@code mapFactory}, populating it with the provided {@code entries}.
+     * @since 1.0
+     */
+    @SafeVarargs
+    public static <K, V> Map<K, V> fromEntries(Supplier<Map<K, V>> mapFactory, @NotNull Map.Entry<K, V>... entries) {
+        return fromEntriesInternal(new MapBuilder<>(mapFactory), entries);
+    }
+
+    @SafeVarargs
+    private static <K, V> Map<K, V> fromEntriesInternal(MapBuilder<K, V> builder, @NotNull Map.Entry<K, V>... entries) {
+        for (Map.Entry<K, V> entry : entries) {
+            builder.put(entry);
+        }
+        return builder.build();
+    }
+
+    /**
+     * {@link Map} entry used for creating {@link Map} using {@link MapBuilder}.
+     * @since 1.0
+     */
+    private static class Entry<K, V> implements Map.Entry<K, V> {
+        private final K key;
+        private V value;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+    }
+}
---
+++ src/main/java/ru/mrgrd56/mgutils/concurrent/TaskInvoker.java
@@ -0,0 +1,153 @@
+package ru.mrgrd56.mgutils.concurrent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+/**
+ * Used for executing a specific set of tasks, distributing them among threads using {@link ExecutorService}.<br><br>
+ * Main methods:<br>
+ * <ul>
+ *     <li>{@link #submit(Runnable)} - accepts a task. The task <u>DOES NOT</u> start executing;</li>
+ *     <li>{@link #completeAll()} - executes all accepted tasks using {@link #invokeAll}, waits for their completion, and returns the results.</li>
+ * </ul>
+ * @param <T> The type of value returned by the executed tasks.
+ * @since 1.0
+ */
+public class TaskInvoker<T> {
+    private final List<InvokerCallable<T>> tasks = Collections.synchronizedList(new ArrayList<>());
+    private final ExecutorService executor;
+
+    public TaskInvoker(ExecutorService executor) {
+        this.executor = executor;
+    }
+
+    /**
+     * Accepts a task by adding it to the list for execution. The task <u>DOES NOT</u> start executing.
+     * @since 1.0
+     */
+    public void submit(Callable<T> task) {
+        tasks.add(new InvokerCallable<>(task));
+    }
+
+    /**
+     * Accepts a task with no return value, adding it to the list for execution. The task <u>DOES NOT</u> start executing.
+     * @since 1.0
+     */
+    public void submit(Runnable task) {
+        submit(() -> {
+            task.run();
+            return null;
+        });
+    }
+
+    /**
+     * Accepts tasks by adding them to the list for execution. The tasks <u>DO NOT</u> start executing.
+     * @see #submit(Callable)
+     * @since 1.0
+     */
+    public void submitAll(Collection<Callable<T>> tasks) {
+        this.tasks.addAll(tasks.stream().map(InvokerCallable::new).collect(Collectors.toList()));
+    }
+
+    /**
+     * Accepts tasks with no return value, adding them to the list for execution. The tasks <u>DO NOT</u> start executing.
+     * @see #submit(Runnable)
+     * @see #submitAll(Collection)
+     * @since 1.0
+     */
+    public void submitAllVoid(Collection<Runnable> tasks) {
+        List<Callable<T>> voidTasks = tasks.stream()
+                .map(task -> {
+                    return (Callable<T>) () -> {
+                        task.run();
+                        return null;
+                    };
+                })
+                .collect(Collectors.toList());
+
+        submitAll(voidTasks);
+    }
+
+    /**
+     * Executes all accepted tasks using {@link ExecutorService#invokeAll}. The list of accepted tasks is cleared.
+     * @since 1.0
+     */
+    public List<Future<T>> invokeAll() {
+        if (tasks.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        try {
+            return executor.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            tasks.clear();
+        }
+    }
+
+    /**
+     * Executes all still uncompleted tasks using {@link #invokeAll}, waits for their completion, and returns the results.<br>
+     * The list of accepted tasks is cleared.
+     * @throws CancellationException Might be thrown if the {@link #cancelAll} method was called while the current tasks were being executed.
+     * @since 1.0
+     */
+    public List<T> completeAll() throws CancellationException {
+        if (tasks.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return invokeAll().stream().map(TaskInvoker::getFutureResult).collect(Collectors.toList());
+    }
+
+    /**
+     * Cancels all the accepted tasks. If attempted to execute, these tasks will throw a {@link CancellationException}.<br>
+     * Multiple calls to the method for the same tasks will not lead to anything.
+     * @since 1.0
+     */
+    public void cancelAll() {
+        for (InvokerCallable<T> task : this.tasks) {
+            task.cancel();
+        }
+    }
+
+    private static <T> T getFutureResult(Future<T> future) throws CancellationException {
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TaskInvoker tasks: %s, executor: %s", tasks.size(), executor.toString());
+    }
+
+    private static class InvokerCallable<T> implements Callable<T> {
+        private final Callable<T> callable;
+        private final AtomicBoolean isCancelled = new AtomicBoolean(false);
+
+        public InvokerCallable(Callable<T> callable) {
+            this.callable = callable;
+        }
+
+        public void cancel() {
+            isCancelled.set(true);
+        }
+
+        @Override
+        public T call() throws Exception {
+            if (isCancelled.get()) {
+                throw new CancellationException("The task has been cancelled");
+            }
+
+            return callable.call();
+        }
+    }
+}
