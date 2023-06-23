b8215d3dd29340b049d30b39bd34a036bdb1f82d
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-23 21:21:46 +0500

    Release v1.5.1 - Remove unnecessary checks in StringUtils

--- src/test/java/ru/mrgrd56/mgutils/StringUtilsTest.java
+++ src/test/java/ru/mrgrd56/mgutils/StringUtilsTest.java
@@ -15,8 +15,29 @@ public class StringUtilsTest {
         Assertions.assertEquals(" lorem  ipsum dolor sit  amet",
                 StringUtils.joinNotBlank(" ", new StringLike(" lorem"), new StringLike(" "), new StringLike(null), null, new StringLike(" ipsum"), new StringLike("dolor"), new StringLike(""), new StringLike(" "), new StringLike("sit "), new StringLike("amet")));
 
-        Assertions.assertEquals("lorem ipsum dolor sit amet",
-                StringUtils.joinNotBlankTrimming(" ", " lorem", null, new StringLike(" "), new StringLike(null), null, new StringLike(" ipsum"), new StringLike("dolor"), new StringLike(""), new StringLike(" "), new StringLike("sit "), new StringLike("amet")));
+        Assertions.assertEquals("lorem,ipsum,dolor,sit,amet",
+                StringUtils.joinNotBlankTrimming(",", " lorem", null, new StringLike(" "), new StringLike(null), null, new StringLike(" ipsum"), new StringLike("dolor"), new StringLike(""), new StringLike(" "), new StringLike("sit "), new StringLike("amet")));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", null, "    ", "", null, "", " ", ""));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlankTrimming(" ", null, new StringLike("    "), "", null, "", " ", ""));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", new Object[0]));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", (String) null));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", (Object) null));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", new StringLike(null)));
+
+        Assertions.assertEquals("",
+                StringUtils.joinNotBlank(" ", new StringLike("  ")));
     }
 
     private static class StringLike {
