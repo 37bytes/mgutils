ce47059330b740c7354ec4be9ba7a5a5422aeb0c
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-23 15:16:20 +0500

    Release v1.4.0 - StringUtils: add new methods on joining

--- src/main/java/ru/mrgrd56/mgutils/StringUtils.java
+++ src/main/java/ru/mrgrd56/mgutils/StringUtils.java
@@ -1,5 +1,6 @@
 package ru.mrgrd56.mgutils;
 
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,6 +19,54 @@ public final class StringUtils {
     public static String joinNotBlank(CharSequence delimiter, CharSequence... strings) {
         return Stream.of(strings)
                 .filter(org.apache.commons.lang3.StringUtils::isNotBlank)
+                .collect(Collectors.joining(delimiter));
+    }
+
+    /**
+     * Joins the {@code objects} as strings that are not empty, placing the {@code delimiter} between them.
+     *
+     * @param delimiter The string separator.
+     * @param objects The strings to be joined.
+     * @since 1.4.0
+     */
+    public static String joinNotBlank(CharSequence delimiter, Object... objects) {
+        return Stream.of(objects)
+                .filter(Objects::nonNull)
+                .map(ObjectUtils::toString)
+                .filter(org.apache.commons.lang3.StringUtils::isNotBlank)
+                .collect(Collectors.joining(delimiter));
+    }
+
+    /**
+     * Joins the {@code strings} that are not empty, placing the {@code delimiter} between them.<br>
+     * Removes unnecessary spaces between substrings.
+     *
+     * @param delimiter The string separator.
+     * @param strings The strings to be joined.
+     * @since 1.4.0
+     */
+    public static String joinNotBlankTrimming(CharSequence delimiter, CharSequence... strings) {
+        return Stream.of(strings)
+                .filter(org.apache.commons.lang3.StringUtils::isNotBlank)
+                .map(CharSequence::toString)
+                .map(String::trim)
+                .collect(Collectors.joining(delimiter));
+    }
+
+    /**
+     * Joins the {@code objects} as strings that are not empty, placing the {@code delimiter} between them.<br>
+     * Removes unnecessary spaces between substrings.
+     *
+     * @param delimiter The string separator.
+     * @param objects The strings to be joined.
+     * @since 1.4.0
+     */
+    public static String joinNotBlankTrimming(CharSequence delimiter, Object... objects) {
+        return Stream.of(objects)
+                .filter(Objects::nonNull)
+                .map(ObjectUtils::toString)
+                .filter(org.apache.commons.lang3.StringUtils::isNotBlank)
+                .map(String::trim)
                 .collect(Collectors.joining(delimiter));
     }
 }
