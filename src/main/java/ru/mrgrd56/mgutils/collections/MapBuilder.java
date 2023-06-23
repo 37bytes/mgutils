b49ed6d50e8fc6eb15c57f18098fe1186db4917c
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-23 13:07:32 +0500

    Release v1.3.0 - MapBuilder: add new methods for conditional building

--- src/main/java/ru/mrgrd56/mgutils/collections/MapBuilder.java
+++ src/main/java/ru/mrgrd56/mgutils/collections/MapBuilder.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
@@ -30,8 +31,20 @@ public class MapBuilder<K, V> {
      * The {@link Map} created by {@code mapFactory} must support the {@link Map#put} operation.
      * @param mapFactory The object that creates a {@link Map} instance.
      */
-    public MapBuilder(Supplier<Map<K, V>> mapFactory) {
-        this.map = mapFactory.get();
+    public MapBuilder(@NotNull Supplier<Map<K, V>> mapFactory) {
+        Objects.requireNonNull(mapFactory);
+        this.map = Objects.requireNonNull(mapFactory.get());
+    }
+
+    /**
+     * Builds the instance of {@link MapBuilder} provided as {@code initialMap}.<br>
+     * The {@link Map} provided in {@code initialMap} must support the {@link Map#put} operation.
+     * @param initialMap The object that is used as an initial map for this builder.
+     * @since 1.3.0
+     */
+    public MapBuilder(@NotNull Map<K, V> initialMap) {
+        Objects.requireNonNull(initialMap);
+        this.map = initialMap;
     }
 
     /**
@@ -45,11 +58,55 @@ public class MapBuilder<K, V> {
     }
 
     /**
-     * Inserts an entry into the created {@link Map}, using the provided {@code key} and {@code value}.
-     * @param entry The entry added to the created {@link Map}.
+     * Inserts an entry into the created {@link Map}, using the provided {@code entry}.
+     * @param entry The entry to be added to the created {@link Map}.
      */
     public MapBuilder<K, V> put(@NotNull Map.Entry<K, V> entry) {
         return put(entry.getKey(), entry.getValue());
+    }
+
+    /**
+     * If the {@code condition} equals to {@code true}, inserts an entry into the created {@link Map}, using the provided {@code key} and {@code value}.
+     * @param condition Condition indicating whether the entry will be retrieved from the {@code entrySupplier} and added to the {@link Map}.
+     * @param key The key associated with the value.
+     * @param value The value.
+     * @since 1.3.0
+     */
+    public MapBuilder<K, V> putIf(boolean condition, @Nullable K key, @Nullable V value) {
+        if (condition) {
+            put(key, value);
+        }
+
+        return this;
+    }
+
+    /**
+     * If the {@code condition} equals to {@code true}, inserts the {@code entry} into the created {@link Map}.
+     * @param condition Condition indicating whether the entry will be retrieved from the {@code entrySupplier} and added to the {@link Map}.
+     * @param entry The entry to be added to the created {@link Map}.
+     * @since 1.3.0
+     */
+    public MapBuilder<K, V> putIf(boolean condition, @NotNull Map.Entry<K, V> entry) {
+        if (condition) {
+            put(entry);
+        }
+
+        return this;
+    }
+
+    /**
+     * If the {@code condition} equals to {@code true}, inserts an entry
+     * into the created {@link Map}, getting the entry from the provided {@code entrySupplier}.
+     * @param condition Condition indicating whether the entry will be retrieved from the {@code entrySupplier} and added to the {@link Map}.
+     * @param entrySupplier Function returning the entry to be added to the created {@link Map}.
+     * @since 1.3.0
+     */
+    public MapBuilder<K, V> putIf(boolean condition, @NotNull Supplier<Map.Entry<K, V>> entrySupplier) {
+        if (condition) {
+            put(entrySupplier.get());
+        }
+
+        return this;
     }
 
     /**
@@ -63,30 +120,47 @@ public class MapBuilder<K, V> {
     /**
      * Creates an {@link Entry} instance.
      */
+    @NotNull
     public static <K, V> Map.Entry<K, V> entry(@Nullable K key, @Nullable V value) {
         return new Entry<>(key, value);
     }
 
     /**
-     * Creates a {@link HashMap} instance, populating it with the provided {@code entries}.
+     * Creates a {@link HashMap} instance, populating it with the provided {@code entries}.<br>
+     * Null entries are ignored.
      */
     @SafeVarargs
-    public static <K, V> Map<K, V> fromEntries(@NotNull Map.Entry<K, V>... entries) {
+    public static <K, V> Map<K, V> fromEntries(@Nullable Map.Entry<K, V>... entries) {
         return fromEntriesInternal(new MapBuilder<>(), entries);
     }
 
     /**
-     * Creates a {@link Map} instance using the specified {@code mapFactory}, populating it with the provided {@code entries}.
+     * Builds a {@link Map} instance using the specified {@code initialMap}, populating it with the provided {@code entries}.<br>
+     * Null entries are ignored.
      */
     @SafeVarargs
-    public static <K, V> Map<K, V> fromEntries(Supplier<Map<K, V>> mapFactory, @NotNull Map.Entry<K, V>... entries) {
+    public static <K, V> Map<K, V> fromEntries(Map<K, V> initialMap, @Nullable Map.Entry<K, V>... entries) {
+        return fromEntriesInternal(new MapBuilder<>(initialMap), entries);
+    }
+
+    /**
+     * Creates a {@link Map} instance using the specified {@code mapFactory}, populating it with the provided {@code entries}.<br>
+     * Null entries are ignored.
+     * @since 1.3.0
+     */
+    @SafeVarargs
+    public static <K, V> Map<K, V> fromEntries(Supplier<Map<K, V>> mapFactory, @Nullable Map.Entry<K, V>... entries) {
         return fromEntriesInternal(new MapBuilder<>(mapFactory), entries);
     }
 
     @SafeVarargs
-    private static <K, V> Map<K, V> fromEntriesInternal(MapBuilder<K, V> builder, @NotNull Map.Entry<K, V>... entries) {
+    private static <K, V> Map<K, V> fromEntriesInternal(MapBuilder<K, V> builder, @Nullable Map.Entry<K, V>... entries) {
+        Objects.requireNonNull(entries);
+
         for (Map.Entry<K, V> entry : entries) {
-            builder.put(entry);
+            if (entry != null) {
+                builder.put(entry);
+            }
         }
         return builder.build();
     }
