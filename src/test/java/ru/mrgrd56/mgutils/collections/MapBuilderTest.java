9ab810ce55f12210eb016ef42c0de7773bad06e9
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-27 20:47:35 +0500

    Release 1.8.0 - Saving original Map type in MapBuilder static methods - Add MapBuilder#buildAs - Add MapBuilder#putAll - Deprecate all the MapBuilder#fromEntries and replace them with new MapBuilder#create and MapBuilder#populate - Minor fixes on JavaDoc and other

--- src/test/java/ru/mrgrd56/mgutils/collections/MapBuilderTest.java
+++ src/test/java/ru/mrgrd56/mgutils/collections/MapBuilderTest.java
@@ -1,12 +1,15 @@
 package ru.mrgrd56.mgutils.collections;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapBuilderTest {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -45,7 +48,7 @@ public class MapBuilderTest {
         Map<String, Object> initialMap = new HashMap<>();
         initialMap.put("init", "42");
 
-        Map<String, Object> map = MapBuilder.fromEntries(initialMap,
+        Map<String, Object> map = MapBuilder.populate(initialMap,
                 MapBuilder.entry("hello", "world"),
                 false ? MapBuilder.entry("never1", "error") : null,
                 MapBuilder.entry("correc", "t"),
@@ -65,7 +68,7 @@ public class MapBuilderTest {
     @Test
     public void testBuildAs() {
         CustomMap customMap = new CustomMap(
-                MapBuilder.fromEntries(
+                MapBuilder.create(
                         MapBuilder.entry("lorem", "ipsum")
                 )
         );
@@ -81,7 +84,7 @@ public class MapBuilderTest {
 
     @Test
     public void testBuildAsError() {
-        Map<String, Object> customMap = MapBuilder.fromEntries(
+        Map<String, Object> customMap = MapBuilder.create(
                 MapBuilder.entry("lorem", "ipsum")
         );
 
@@ -95,6 +98,101 @@ public class MapBuilderTest {
             // expected!
             log.info("ClassCastException thrown (success)", e);
         }
+    }
+
+    @Test
+    public void testAutoCasting() {
+        CustomMap customMap = new CustomMap(
+                MapBuilder.create(
+                        MapBuilder.entry("lorem", "ipsum")
+                )
+        );
+
+        CustomMap customMap2 = MapBuilder.populate(customMap,
+                MapBuilder.entry("lorem", "sit amet"));
+
+        Assertions.assertInstanceOf(CustomMap.class, customMap2);
+        Assertions.assertSame(customMap, customMap2);
+        Assertions.assertEquals("sit amet", customMap2.getLorem());
+
+        HashMap<String, Object> customMap3 = MapBuilder.populate(customMap,
+                MapBuilder.entry("lorem3", "dolor sit amet"));
+
+        Assertions.assertInstanceOf(HashMap.class, customMap3);
+        Assertions.assertSame(customMap, customMap3);
+        Assertions.assertEquals("dolor sit amet", customMap3.get("lorem3"));
+        Assertions.assertEquals("sit amet", customMap3.get("lorem"));
+    }
+
+    @Test
+    public void testAutoCasting2() {
+        ConcurrentHashMap<String, Object> map = MapBuilder.create(ConcurrentHashMap::new,
+                MapBuilder.entry("one", 1),
+                MapBuilder.entry("two", 2),
+                MapBuilder.entry("three", 3));
+
+        Map<String, Object> map2 = MapBuilder.populate(new ConcurrentHashMap<>(map),
+                MapBuilder.entry("lorem2_1", "sit amet 2 1"),
+                MapBuilder.entry("lorem2_2", "sit amet 2 2"));
+
+        ConcurrentHashMap<String, Object> map3 = MapBuilder.populate(map,
+                MapBuilder.entry("lorem3", "sit amet"));
+
+        Assertions.assertInstanceOf(ConcurrentHashMap.class, map);
+        Assertions.assertInstanceOf(ConcurrentHashMap.class, map2);
+        Assertions.assertInstanceOf(ConcurrentHashMap.class, map3);
+
+        Assertions.assertSame(map, map3);
+        Assertions.assertNotSame(map, map2);
+        Assertions.assertEquals("sit amet 2 2", map2.get("lorem2_2"));
+        Assertions.assertEquals("sit amet", map3.get("lorem3"));
+    }
+
+    @Test
+    public void testPutAll() {
+        Map<String, Object> map = new MapBuilder<String, Object>(
+                MapBuilder.create(
+                        MapBuilder.entry("Hello", "World")
+                )
+        )
+                .put("World", "sekai")
+                .putAll(
+                        MapBuilder.create(LinkedHashMap::new,
+                                MapBuilder.entry("h", "e")
+                        )
+                )
+                .build();
+
+        Assertions.assertEquals("e", map.get("h"));
+        Assertions.assertEquals("sekai", map.get("World"));
+        Assertions.assertEquals("World", map.get("Hello"));
+    }
+
+    @Test
+    public void testCastingPerformance() {
+        StopWatch stopWatch1 = StopWatch.createStarted();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            Map<String, Object> customMap = MapBuilder.create(
+                    MapBuilder.entry("lorem", "ipsum")
+            );
+        }
+
+        log.info("NO_CASTING took {}", stopWatch1.formatTime());
+
+        StopWatch stopWatch2 = StopWatch.createStarted();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            Map<String, Object> customMap = MapBuilder.create(HashMap::new,
+                    MapBuilder.entry("lorem", "ipsum")
+            );
+        }
+
+        log.info("CASTING took {}", stopWatch2.formatTime());
+    }
+
+    private HashMap<String, Object> createMap() {
+        return new HashMap<>();
     }
 
     private static class CustomMap extends HashMap<String, Object> {
