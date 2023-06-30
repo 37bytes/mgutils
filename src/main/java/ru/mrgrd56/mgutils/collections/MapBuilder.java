10f7c8c97eaa76a60b59cf5211eaf28d8f1ae448
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-30 14:13:07 +0500

    Release 1.9.0 - Add more methods to StringUtils (char replacement) - Add ExceptionUtils#asRuntimeException - Add pending (not released) PerformanceTest, it's in development yet, as well as new MapBuilder methods - Add MapBuilder#buildUnmodifiable

--- src/main/java/ru/mrgrd56/mgutils/collections/MapBuilder.java
+++ src/main/java/ru/mrgrd56/mgutils/collections/MapBuilder.java
@@ -201,45 +201,6 @@ public class MapBuilder<K, V> {
         return populateBuilder(new MapBuilder<>(mapFactory), entries).buildAs();
     }
 
-    /**
-     * Creates a {@link HashMap} instance, populating it with the provided {@code entries}.<br>
-     * Null entries are ignored.
-     * @since 1.9.0
-     */
-    @SafeVarargs
-    public static <K, V> Map<K, V> createAsUnmodifiable(@Nullable Map.Entry<K, V>... entries) {
-        // TODO v1.9.0 choose better names and update the JavaDoc
-        return populateBuilder(new MapBuilder<>(), entries).buildUnmodifiable();
-    }
-
-    /**
-     * Builds a {@link Map} instance using the specified {@code initialMap}, populating it with the provided {@code entries}.<br>
-     * Null entries are ignored.<br>
-     * It's guaranteed that the {@link Map} object returned by {@link #build()}
-     * will have the same reference as the {@code initialMap}.<br>
-     * <br>
-     * Since 1.8.0 the replacement for {@link #fromEntries(Map, Map.Entry[])}.
-     * @since 1.9.0
-     */
-    @SafeVarargs
-    public static <K, V> Map<K, V> populateAsUnmodifiable(@NotNull Map<K, V> initialMap, @Nullable Map.Entry<K, V>... entries) {
-        // TODO v1.9.0 choose better names and update the JavaDoc
-        return populateBuilder(new MapBuilder<>(initialMap), entries).buildUnmodifiable();
-    }
-
-    /**
-     * Creates a {@link Map} instance using the specified {@code mapFactory}, populating the returning {@link Map} with the provided {@code entries}.<br>
-     * Null entries are ignored.<br>
-     * <br>
-     * Since 1.8.0 the replacement for {@link #fromEntries(Supplier, Map.Entry[])}.
-     * @since 1.9.0
-     */
-    @SafeVarargs
-    public static <K, V> Map<K, V> createAsUnmodifiable(@NotNull Supplier<Map<K, V>> mapFactory, @Nullable Map.Entry<K, V>... entries) {
-        // TODO v1.9.0 choose better names and update the JavaDoc
-        return populateBuilder(new MapBuilder<>(mapFactory), entries).buildUnmodifiable();
-    }
-
     // region deprecated fromEntries
     /**
      * Creates a {@link HashMap} instance, populating it with the provided {@code entries}.<br>
