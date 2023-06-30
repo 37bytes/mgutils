08f3ac6102f8f460cd909f4b53b3dc36a5160c53
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-06-30 14:10:20 +0500

    Release 1.9.0 - Add more methods to StringUtils (char replacement) - Add ExceptionUtils#asRuntimeException - Add pending (not released) PerformanceTest, it's in development yet

--- src/main/java/ru/mrgrd56/mgutils/StringUtils.java
+++ src/main/java/ru/mrgrd56/mgutils/StringUtils.java
@@ -1,5 +1,7 @@
 package ru.mrgrd56.mgutils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,6 +12,22 @@ public final class StringUtils {
     private StringUtils() { }
 
     /**
+     * Replaces the character by {@code charIndex} in the {@code string} with the character {@code replacement}.
+     *
+     * @param string The original string in which the replacement needs to be performed.
+     * @param charIndex The index of the char to be replaced.
+     * @param replacement The character to replace with.
+     * @return The string with the character replaced.
+     * @since 1.9.0
+     */
+    @NotNull
+    public static String setCharAt(@NotNull String string, int charIndex, char replacement) {
+        char[] chars = string.toCharArray();
+        chars[charIndex] = replacement;
+        return String.valueOf(chars);
+    }
+
+    /**
      * Replaces the first occurrence of the character {@code oldChar} in the {@code string} with the character {@code newChar}.
      *
      * @param string  The original string in which the replacement needs to be performed.
@@ -18,16 +36,35 @@ public final class StringUtils {
      * @return The string with the character replaced.
      * @since 1.9.0
      */
-    public static String replaceOnce(String string, char oldChar, char newChar) {
+    @NotNull
+    public static String replaceOnce(@NotNull String string, char oldChar, char newChar) {
         int index = string.indexOf(oldChar);
 
         if (index == -1) {
             return string;
         }
 
-        char[] chars = string.toCharArray();
-        chars[index] = newChar;
-        return String.valueOf(chars);
+        return setCharAt(string, index, newChar);
+    }
+
+    /**
+     * Replaces the last occurrence of the character {@code oldChar} in the {@code string} with the character {@code newChar}.
+     *
+     * @param string  The original string in which the replacement needs to be performed.
+     * @param oldChar The character that needs to be replaced.
+     * @param newChar The character to replace with.
+     * @return The string with the character replaced.
+     * @since 1.9.0
+     */
+    @NotNull
+    public static String replaceLastOnce(@NotNull String string, char oldChar, char newChar) {
+        int index = string.lastIndexOf(oldChar);
+
+        if (index == -1) {
+            return string;
+        }
+
+        return setCharAt(string, index, newChar);
     }
 
     /**
