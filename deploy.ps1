50cd9993651713a13a22065560eac4774243a8ae
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-07-06 01:30:07 +0500

    Prepare for deployment to GitHub Packages 6

--- deploy.ps1
+++ deploy.ps1
@@ -42,7 +42,7 @@ if ([string]::IsNullOrEmpty($existedInRemote)) {
 }
 
 # Use version 2.5.2 specifically, as later versions do not generate checksums (removed in versions 3.0.0+)
-mvn -f "../pom.xml" install:2.5.2:install-file -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dfile=$jarLocation -Dpackaging=jar -DgeneratePom=$true "-DlocalRepositoryPath=." -DcreateChecksum=$true
+mvn -f "../pom.xml" "install:2.5.2:install-file" -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dfile=$jarLocation -Dpackaging=jar -DgeneratePom=$true "-DlocalRepositoryPath=." -DcreateChecksum=$true
 
 # Commit and push
 Write-Host "Committing and pushing to repository branch..."
