ed4bfb2a1f5eeed47e7139dbfec2f0ee9585d91c
KIRILL GOLIKOV <bukinist85@gmail.com>
2023-08-12 21:47:13 +0500

    Changes on LockStore 2

--- src/test/java/dev/b37/mgutils/concurrent/lock/LockStoreTests.java
+++ src/test/java/dev/b37/mgutils/concurrent/lock/LockStoreTests.java
@@ -1,18 +1,19 @@
 package dev.b37.mgutils.concurrent.lock;
 
+import com.google.common.util.concurrent.Striped;
 import dev.b37.mgutils.RandomUtils;
+import dev.b37.mgutils.collections.ImmutableMapBuilder;
 import dev.b37.mgutils.concurrent.TaskInvoker;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -195,12 +196,14 @@ public class LockStoreTests {
 
     @RepeatedTest(5)
     public void testLockStoreMultipleTasks() throws InterruptedException {
+        final int idsCount = 3;
+
         List<Task> tasks = new ArrayList<>(100);
         for (int i = 0; i < 1000; i++) {
             Task task = new Task();
             task.id = UUID.randomUUID();
             task.type = RandomUtils.nextItem(TaskType.values());
-            task.entityId = org.apache.commons.lang3.RandomUtils.nextInt(0, 2);
+            task.entityId = org.apache.commons.lang3.RandomUtils.nextInt(0, idsCount);
             tasks.add(task);
         }
 
@@ -208,7 +211,12 @@ public class LockStoreTests {
         TaskInvoker<Void> invoker = new TaskInvoker<>(executor);
 
         LockStore lockStore = new LockStore();
-        Map<Integer, Integer> entityUsagesMap = new ConcurrentHashMap<>();
+
+        ImmutableMapBuilder<Integer, Queue<Task>> mapBuilder = new ImmutableMapBuilder<>(ConcurrentHashMap::new);
+        for (int i = 0; i < idsCount; i++) {
+            mapBuilder.put(i, new LinkedBlockingQueue<>());
+        }
+        Map<Integer, Queue<Task>> entityUsagesArray = mapBuilder.build();
 
         for (Task task : tasks) {
             invoker.submit(() -> {
@@ -219,13 +227,8 @@ public class LockStoreTests {
                 }
 
                 try (KeyLock lock = lockStore.lock(task.entityId)) {
-                    Integer usagesByEntity = entityUsagesMap.compute(task.entityId, (k, v) -> {
-                        if (v == null) {
-                            return 1;
-                        } else {
-                            return v + 1;
-                        }
-                    });
+                    Queue<Task> usagesByEntity = entityUsagesArray.get(task.entityId);
+                    usagesByEntity.add(task);
 
                     try {
                         Thread.sleep(org.apache.commons.lang3.RandomUtils.nextLong(1, 5));
@@ -233,23 +236,20 @@ public class LockStoreTests {
                         throw new RuntimeException(e);
                     }
 
-                    Assertions.assertEquals(1, (int) usagesByEntity, () -> {
+                    Assertions.assertEquals(1, usagesByEntity.size(), () -> {
                         return "there are unexpected usages (" + usagesByEntity + ") of current usagesByEntity " +
-                               entityUsagesMap + " " + lockStore;
+                               entityUsagesArray + " " + lockStore;
                     });
 
-                    Assertions.assertTrue(entityUsagesMap.values().stream().allMatch(usages -> usages <= 1), () -> {
-                        return "there are unexpected usages of entityUsagesMapByTypes " + entityUsagesMap + " " +
+                    Assertions.assertTrue(entityUsagesArray.values().stream().allMatch(usages -> usages.size() <= 1), () -> {
+                        return "there are unexpected usages of entityUsagesMapByTypes " + entityUsagesArray + " " +
                                lockStore;
                     });
 
-                    entityUsagesMap.compute(task.entityId, (k, v) -> {
-                        if (v == null) {
-                            throw new IllegalStateException();
-                        } else {
-                            return v - 1;
-                        }
-                    });
+                    Task removedTask = usagesByEntity.remove();
+                    if (removedTask != task) {
+                        Assertions.fail("removed task is not the same as the added. " + task + " -> " + removedTask);
+                    }
                 }
             });
         }
@@ -257,7 +257,7 @@ public class LockStoreTests {
         invoker.completeAllVoid();
         executor.shutdown();
 
-        Assertions.assertTrue(entityUsagesMap.values().stream().allMatch(usages -> usages == 0));
+        Assertions.assertTrue(entityUsagesArray.values().stream().allMatch(Collection::isEmpty));
 
         try {
             Map lockMap = (Map) FieldUtils.readField(lockStore, "lockMap", true);
@@ -266,6 +266,147 @@ public class LockStoreTests {
             throw new RuntimeException(e);
         }
     }
+
+    @RepeatedTest(5)
+    public void testStripedMultipleTasks() throws InterruptedException {
+        final int idsCount = 3;
+
+        List<Task> tasks = new ArrayList<>(100);
+        for (int i = 0; i < 1000; i++) {
+            Task task = new Task();
+            task.id = UUID.randomUUID();
+            task.type = RandomUtils.nextItem(TaskType.values());
+            task.entityId = org.apache.commons.lang3.RandomUtils.nextInt(0, idsCount);
+            tasks.add(task);
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(64);
+        TaskInvoker<Void> invoker = new TaskInvoker<>(executor);
+
+        Striped<Lock> lockStore = Striped.lock(idsCount);
+
+        ImmutableMapBuilder<Integer, Queue<Task>> mapBuilder = new ImmutableMapBuilder<>(ConcurrentHashMap::new);
+        for (int i = 0; i < idsCount; i++) {
+            mapBuilder.put(i, new LinkedBlockingQueue<>());
+        }
+        Map<Integer, Queue<Task>> entityUsagesArray = mapBuilder.build();
+
+        for (Task task : tasks) {
+            invoker.submit(() -> {
+                try {
+                    Thread.sleep(org.apache.commons.lang3.RandomUtils.nextLong(10, 100));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Lock lock = lockStore.get(task.entityId);
+                lock.lock();
+                try {
+                    Queue<Task> usagesByEntity = entityUsagesArray.get(task.entityId);
+                    usagesByEntity.add(task);
+
+                    try {
+                        Thread.sleep(org.apache.commons.lang3.RandomUtils.nextLong(1, 5));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    Assertions.assertEquals(1, usagesByEntity.size(), () -> {
+                        return "there are unexpected usages (" + usagesByEntity + ") of current usagesByEntity " +
+                               entityUsagesArray + " " + lockStore;
+                    });
+
+                    Assertions.assertTrue(entityUsagesArray.values().stream().allMatch(usages -> usages.size() <= 1), () -> {
+                        return "there are unexpected usages of entityUsagesMapByTypes " + entityUsagesArray + " " +
+                               lockStore;
+                    });
+
+                    Task removedTask = usagesByEntity.remove();
+                    if (removedTask != task) {
+                        Assertions.fail("removed task is not the same as the added. " + task + " -> " + removedTask);
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            });
+        }
+
+        invoker.completeAllVoid();
+        executor.shutdown();
+
+        Assertions.assertTrue(entityUsagesArray.values().stream().allMatch(Collection::isEmpty));
+    }
+
+    @RepeatedTest(5)
+    public void testLocksMapMultipleTasks() throws InterruptedException {
+        final int idsCount = 3;
+
+        List<Task> tasks = new ArrayList<>(100);
+        for (int i = 0; i < 1000; i++) {
+            Task task = new Task();
+            task.id = UUID.randomUUID();
+            task.type = RandomUtils.nextItem(TaskType.values());
+            task.entityId = org.apache.commons.lang3.RandomUtils.nextInt(0, idsCount);
+            tasks.add(task);
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(64);
+        TaskInvoker<Void> invoker = new TaskInvoker<>(executor);
+
+        Map<Object, Lock> lockStore = new ConcurrentHashMap<>();
+
+        ImmutableMapBuilder<Integer, Queue<Task>> entityUsagesMapBuilder = new ImmutableMapBuilder<>(ConcurrentHashMap::new);
+        for (int i = 0; i < idsCount; i++) {
+            entityUsagesMapBuilder.put(i, new LinkedBlockingQueue<>());
+        }
+        Map<Integer, Queue<Task>> entityUsagesMap = entityUsagesMapBuilder.build();
+
+        for (Task task : tasks) {
+            invoker.submit(() -> {
+                try {
+                    Thread.sleep(org.apache.commons.lang3.RandomUtils.nextLong(10, 100));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Lock lock = lockStore.computeIfAbsent(task.entityId, k -> new ReentrantLock());
+                lock.lock();
+                try {
+                    Queue<Task> usagesByEntity = entityUsagesMap.get(task.entityId);
+                    usagesByEntity.add(task);
+
+                    try {
+                        Thread.sleep(org.apache.commons.lang3.RandomUtils.nextLong(1, 5));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    Assertions.assertEquals(1, usagesByEntity.size(), () -> {
+                        return "there are unexpected usages (" + usagesByEntity + ") of current usagesByEntity " +
+                               entityUsagesMap + " " + lockStore;
+                    });
+
+                    Assertions.assertTrue(entityUsagesMap.values().stream().allMatch(usages -> usages.size() <= 1), () -> {
+                        return "there are unexpected usages of entityUsagesMapByTypes " + entityUsagesMap + " " +
+                               lockStore;
+                    });
+
+                    Task removedTask = usagesByEntity.remove();
+                    if (removedTask != task) {
+                        Assertions.fail("removed task is not the same as the added. " + task + " -> " + removedTask);
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            });
+        }
+
+        invoker.completeAllVoid();
+        executor.shutdown();
+
+        Assertions.assertTrue(entityUsagesMap.values().stream().allMatch(Collection::isEmpty));
+    }
+
 
     @RepeatedTest(10)
     public void testLockStoreMultipleTasksExtended() throws InterruptedException {
